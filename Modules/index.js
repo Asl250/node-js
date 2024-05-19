ad464ed0f5edced1626240b68c45ae26851d0fbd
Asl250 <aslrahidov250@gmail.com>
2024-05-19 22:01:07 +0500

    module wrapper, modules, ES6

---
+++ Modules/index.js
@@ -0,0 +1,7 @@
+const hello = require('./hello')
+const {person1, person2, person3 } = require('./people')
+
+hello(person1)
+hello(person2)
+hello(person3)
+
---
+++ Modules/people.js
@@ -0,0 +1,9 @@
+let person1 = 'kumanboy'
+let person2 = 'ask250'
+let person3 = 'm1les'
+
+module.exports = {
+	person1,
+	person2,
+	person3
+}
